41affca88b9b34d46777e4723f51d543fcb4cc58
Zack-good <160092406+Zack-good@users.noreply.github.com>
2025-02-10 10:36:38 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-int main() {
-    cout << "Hello, World!" << endl;
+int main(){
+    int n;
+    cin>>n;
+    cout<<"8"<<endl;
+    cout<<(n-2)*12<<endl;
+    cout<<(n-2)*(n-2)*6<<endl;
     return 0;
 }
